9fff4b678217c0cd1311e69f19e0abac05216615
mengyanshou <44189674+mengyanshou@users.noreply.github.com>
2022-06-10 15:10:39 +0800

    Update directory_listing.dart

--- pkgs/shelf_static/lib/src/directory_listing.dart
+++ pkgs/shelf_static/lib/src/directory_listing.dart
@@ -5,7 +5,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:path/path.dart' as path;
 import 'package:shelf/shelf.dart';
 
@@ -13,50 +12,57 @@ String _getHeader(String sanitizedHeading) => '''<!DOCTYPE html>
 <html>
 <head>
   <title>Directory listing for $sanitizedHeading</title>
-  <style>
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: sans-serif;
-  }
-  h1 {
-    background-color: #4078c0;
-    color: white;
-    font-weight: normal;
-    margin: 0 0 10px 0;
-    padding: 16px 32px;
-    white-space: nowrap;
-  }
-  ul {
-    margin: 0;
-  }
-  li {
-    padding: 0;
-  }
-  a {
-    line-height: 1.4em;
-  }
-  </style>
+      <script>
+        function onClick(data) {
+            window.open(data)
+        }
+    </script>
+    <style>
+        button{
+            margin: 0 0 0 60px;
+        }
+    </style>
 </head>
 <body>
-  <h1>$sanitizedHeading</h1>
-  <ul>
+<h1>Index of $sanitizedHeading</h1>
+<table>
+  <tr>
+    <td>Name</td>
+    <td>Last modified</td>
+    <td>Size</td>
+  </tr>
 ''';
 
-const String _trailer = '''  </ul>
+const String _trailer = '''</table>
 </body>
 </html>
 ''';
+int fileNodeCompare(FileSystemEntity e1, FileSystemEntity e2) {
+  if (e1 is Directory && e2 is! Directory) {
+    return -1;
+  }
+  if (e1 is! Directory && e2 is Directory) {
+    return 1;
+  }
+  return e1.path.toLowerCase().compareTo(e2.path.toLowerCase());
+}
 
 Response listDirectory(String fileSystemPath, String dirPath) {
   final controller = StreamController<List<int>>();
   const encoding = Utf8Codec();
   const sanitizer = HtmlEscape();
 
-  void add(String string) {
-    controller.add(encoding.encode(string));
+  void add(String name, String? modified, String? size, bool isDir) {
+    modified ??= '';
+    size ??= '-';
+    String button =
+        isDir ? '' : '<button onclick="onClick(\'$name?download=true\')">直接下载</button>';
+    var entry = '''  <tr>
+    <td><a href="$name">$name</a>$button</td>
+    <td>$modified</td>
+    <td style="text-align: right">$size</td>
+  </tr>''';
+    controller.add(encoding.encode(entry));
   }
 
   var heading = path.relative(dirPath, from: fileSystemPath);
@@ -66,30 +72,37 @@ Response listDirectory(String fileSystemPath, String dirPath) {
     heading = '/$heading/';
   }
 
-  add(_getHeader(sanitizer.convert(heading)));
+  controller.add(encoding.encode(_getHeader(sanitizer.convert(heading))));
 
   // Return a sorted listing of the directory contents asynchronously.
-  Directory(dirPath).list().toList().then((entities) {
-    entities.sort((e1, e2) {
-      if (e1 is Directory && e2 is! Directory) {
-        return -1;
+  Directory(dirPath).list().toList()
+    .then((entities) {
+      entities.sort(fileNodeCompare);
+      if (dirPath != '/') {
+        add('../', null, null, true);
       }
-      if (e1 is! Directory && e2 is Directory) {
-        return 1;
+      for (var entity in entities) {
+        var name = path.relative(entity.path, from: dirPath);
+        var stat = entity.statSync();
+        bool isDir = false;
+        int size = stat.size;
+        String modified = stat.modified.toString();
+        if (entity is Directory) {
+          name += '/';
+          isDir = true;
+        }
+        final sanitizedName = sanitizer.convert(name);
+        var encodedSize = const HtmlEscape().convert(
+          size.toString(),
+        );
+        var encodedModified = const HtmlEscape().convert(modified);
+
+        add(sanitizedName, encodedModified, encodedSize, isDir);
       }
-      return e1.path.compareTo(e2.path);
+
+      controller.add(encoding.encode(_trailer));
+      controller.close();
     });
-
-    for (var entity in entities) {
-      var name = path.relative(entity.path, from: dirPath);
-      if (entity is Directory) name += '/';
-      final sanitizedName = sanitizer.convert(name);
-      add('    <li><a href="$sanitizedName">$sanitizedName</a></li>\n');
-    }
-
-    add(_trailer);
-    controller.close();
-  });
 
   return Response.ok(
     controller.stream,
